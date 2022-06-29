c919fe6746dcace6903e82c23596f71bdf1a39d1
AndresFG23 <105888318+AndresFG23@users.noreply.github.com>
2022-06-28 22:06:19 -0600

    Add files via upload

---
+++ Proyecto-Progra-main/Proyecto/src/proyecto/Catalogo.java
@@ -0,0 +1,21 @@
+package proyecto;
+import javax.swing.JOptionPane;
+public class Catalogo {
+  public void catalog(){
+      byte opcion01=0;
+      JOptionPane.showInputDialog(null,
+             "---Elija un libro----\n"
+            + "Aqui el Catalogo, ofrecemos libros de texto electronicos...\n"
+            + "1-La Biblia(religiones judía y cristiana, transmite la palabra de Dios.)\n"
+            + "2-El Señor de los Anillos( novela de fantasía épica)\n"
+            + "3-El Código da Vinci(novela de misterio)\n"
+            + "4-Piense y hágase rico( el método más famoso y efectivo para hacer dinero)\n"
+            + "Digite el libro para desear ver: ");
+            switch(opcion01){
+                
+                    
+            }
+            
+                
+    } 
+}
---
+++ Proyecto-Progra-main/Proyecto/src/proyecto/Main.java
@@ -0,0 +1,8 @@
+package proyecto;
+import javax.swing.JOptionPane;
+public class Main {
+   public static void main(String[] args) {
+      Menu Menu01=new Menu();
+      Menu01.Menu();
+   }
+}
