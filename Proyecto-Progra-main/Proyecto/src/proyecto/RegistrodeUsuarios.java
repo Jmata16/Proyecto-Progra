02621efa8b177bddd363a315865888feb7832bad
Jose Daniel Mata <105888177+Jmata16@users.noreply.github.com>
2022-06-29 18:01:10 -0600

    Cambios en registro de usuarios

--- Proyecto-Progra-main/Proyecto/src/proyecto/RegistrodeUsuarios.java
+++ Proyecto-Progra-main/Proyecto/src/proyecto/RegistrodeUsuarios.java
@@ -2,40 +2,7 @@ package proyecto;
 
 import javax.swing.JOptionPane;
 public class RegistrodeUsuarios {
-  public void Registro(){
-       byte opcion=0;
-
-
-   do{
-      opcion=Byte.parseByte(JOptionPane.showInputDialog(
-     "          Menú de Usuarios  \n"
-    + "1    Registrar un Usuario\n"
-    + "2    Ver usuarios\n"
-    + "3    Inactivar usuarios\n"
-    + "4    Regresar al menu principal\n"
-    + "Elija una opción"));
-      switch(opcion){
-         case 1:
-            RegistrodeUsuarios usuario01=new RegistrodeUsuarios();
-            usuario01.datosUsuario();
-            break;
-         case 2:
-            RegistrodeUsuarios usuario02=new RegistrodeUsuarios();
-             usuario02.verUsuario();
-            break;
-         case 3:
-             JOptionPane.showMessageDialog(null, "Aqui va Inactivar usuarios");;
-            break;
-         case 4:
-             opcion=4;
-             break;
-         default:
-            JOptionPane.showMessageDialog(null, "Opción no valida");
-            break;  
-      }
-   }while(opcion!=4); 
-    }
-//datos
+    //datos
     private String nombre;
     private String apellidos;
     private String nickname;
@@ -46,35 +13,66 @@ public class RegistrodeUsuarios {
     this.nickname="";
     this.contraseña="";
     } 
+    public String getNombre() {
+        return nombre;
+    }
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+    public String getApellidos() {
+        return apellidos;
     }
     public void setApellidos(String apellidos) {
         this.apellidos = apellidos;
     }
-    public void setnickname(String nickname) {
+    public String getNickname() {
+        return nickname;
+    }
+    public void setNickname(String nickname) {
         this.nickname = nickname;
-    }   
-    public void setcontraseña(String contraseña) {
+    }
+    public String getContraseña() {
+        return contraseña;
+    }
+    public void setContraseña(String contraseña) {
         this.contraseña = contraseña;
-    }          
-//sistema de registro de usuarios   
-  public void datosUsuario(){
-        nombre=JOptionPane.showInputDialog(null, "Digite su nombre: ");
-        apellidos=JOptionPane.showInputDialog(null, "Digite su apellidos: ");
-        contraseña=JOptionPane.showInputDialog(null, "Digite un nickname: ");
-        nickname=JOptionPane.showInputDialog(null, "Digite su contraseña: ");
-        JOptionPane.showMessageDialog(null, "El Usuario de "+nombre+" "+ apellidos
-                +" ha sido registrado bajo el nombre de usuario:"+nickname);
     }
-//sistema de vista de usuarios
-   public void verUsuario(){
-       JOptionPane.showMessageDialog(null, "Pronto el sistema para ver los usuarios registrados ");
+  public void Registro(){
+       byte opcion=0;
+   do{
+      opcion=Byte.parseByte(JOptionPane.showInputDialog(
+     "          Menú de Usuarios  \n"
+    + "1    Registrar un Usuario\n"
+    + "2    Ver usuarios\n"
+    + "3    Inactivar usuarios\n"
+    + "4    Regresar al menu principal\n"
+    + "Elija una opción"));
+      switch(opcion){
+         case 1:
+            //sistema de registro de usuarios 
+            nombre=JOptionPane.showInputDialog(null, "Digite su nombre: ");
+            apellidos=JOptionPane.showInputDialog(null, "Digite su apellidos: ");
+            nickname=JOptionPane.showInputDialog(null, "Digite un nickname: ");
+            contraseña=JOptionPane.showInputDialog(null, "Digite su contraseña: ");
+            JOptionPane.showMessageDialog(null, "El Usuario de "+nombre+" "+ apellidos
+                +" ha sido registrado bajo el nombre de usuario: "+nickname);             
+            break;
+         case 2: 
+             //sistema de vista de usuarios
+            JOptionPane.showMessageDialog(null, "Nombre: "+nombre+"Apellidos: "+ apellidos
+                +" nombre de usuario: "+nickname);
+            break;
+         case 3:
+              //sistema de inactivar usuarios 
+             JOptionPane.showMessageDialog(null, "Pronto el sistema de inactivar usuarios:");
+            break;
+         case 4:
+             opcion=4;
+             break;
+         default:
+            JOptionPane.showMessageDialog(null, "Opción no valida");
+            break;  
+      }
+   }while(opcion!=4); 
     }
-   
- //sistema de inactivar usuarios 
-   public void inactivarUsuarios(){
-       JOptionPane.showMessageDialog(null, "Pronto el sistema de inactivar usuarios:");
-    }  
-   
 }
